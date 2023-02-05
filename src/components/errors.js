664f7c23515e913c4359ce63a10263068a6871d8
Cleiton S <cleiton.pax@gmail.com>
2023-02-05 03:08:45 -0300

    fix for erros display

---
+++ src/components/errors.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Errors = ({ showStage, historic, isError, message }) => {
+  const onlyLetters = (value) => {
+    return value.replace(/[^a-zA-Z]/g, "");
+  };
+  const historyLetters = onlyLetters(historic);
+  const messageLetters = onlyLetters(message);
+  const wrongLetters = () => {
+    return historyLetters.split("").filter(letter => {
+      return !messageLetters.includes(letter);
+    });
+  };
+
+  return (
+    <>
+      {showStage && (
+        <>
+          <p className={"lead mb-0 " + isError}>
+           {wrongLetters.length} of 8 erros
+          </p>
+          <p className="lead text-muted">({historic}...)</p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Errors;
--- src/components/stage.js
+++ src/components/stage.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
+import Errors from "./errors";
 
 const Stage = ({ message, setSessionId, sessionId, setMessage }) => {
   const [historic, setHistoric] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [placeholder, setPlaceholder] = useState("");
+  const [isError, setIsError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const letter = String(inputValue);
+    setInputValue("");
     setIsLoading(true);
     setHistoric(historic + inputValue + ", ");
 
@@ -18,18 +22,23 @@ const Stage = ({ message, setSessionId, sessionId, setMessage }) => {
       },
       body: JSON.stringify({
         session: sessionId,
-        letter: inputValue,
+        letter,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
+        if (!data.message.includes(letter)) {
+          setIsError(" blink ");
+          setTimeout(() => {
+            setIsError("");
+          }, 3000);
+        }
         setIsLoading(false);
         setSessionId(data.session_id);
         setMessage(data.message);
         setInputValue("");
       })
       .catch((error) => {
-        console.error(error);
         setIsLoading(false);
       });
   };
@@ -44,17 +53,17 @@ const Stage = ({ message, setSessionId, sessionId, setMessage }) => {
       setInputValue("");
     }
   };
-  const onlyLetters = (value) => {
-    return value.replace(/[^a-zA-Z]/g, "");
-  };
   const isElementActive = (isActive) => {
     return !isActive ? "-light" : "-primary text-white";
   };
   const isValueEmpty = inputValue.length === 0;
+  const showStage = message?.length !== 0;
 
   React.useEffect(() => {
     const interval = setInterval(() => {
-      if (inputValue.length === 0) {
+      if (isLoading) {
+        setPlaceholder("");
+      } else if (inputValue.length === 0) {
         setPlaceholder(placeholder === "_" ? "" : "_");
       }
     }, 500);
@@ -64,38 +73,54 @@ const Stage = ({ message, setSessionId, sessionId, setMessage }) => {
 
   return (
     <div>
-      <div className="card mb-5 pt-5" style={{"--bs-card-bg": "#282c31"}}>
-        <h1 className="mb-5">{message}</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group d-flex flex-column mb-5">
-            <input
-              className={
-                "h1 text-uppercase form-control border-0 form-control-lg mb-3 mx-auto text-center bg" +
-                isElementActive(isValueEmpty) + isDisabled}
-              type="text"
-              value={inputValue}
-              onChange={(event) => onChangeHandler(event.target.value)}
-              style={{ width: "160px", height: "160px", fontSize: "60px" }}
-              placeholder={placeholder}
-              title="Type a letter"
-            />
-            <button
-              className={
-                "btn btn-lg fw-bold border-0 py-3 mx-auto btn" + isElementActive(!isValueEmpty) +
-                noValue}
-              type="submit"
-              disabled={isLoading || isValueEmpty}
-              title="Try"
-            >
-              Guess a letter
-            </button>
+      <div className="card mb-5 pt-5" style={{ "--bs-card-bg": "#282c31", height: "440px" }}>
+        {!showStage ? (
+          <div className="d-flex justify-content-center py-5 my-5">
+            <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw my-5"></i>
           </div>
-        </form>
+        ) : (
+          <>
+            <h1 className="mb-5">{message}</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group d-flex flex-column mb-5 position-relative">
+                {
+                  isLoading && (
+                    <div className="position-absolute top-0 end-50" style={{transform: "translateX(30px)"}}>
+                      <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw my-5"></i>
+                    </div>
+                  )
+                }
+                <input
+                  className={
+                    "h1 text-uppercase form-control border-0 form-control-lg mb-3 mx-auto text-center bg" +
+                    isElementActive(isValueEmpty) +
+                    isDisabled
+                  }
+                  type="text"
+                  value={inputValue}
+                  onChange={(event) => onChangeHandler(event.target.value)}
+                  style={{ width: "143px", height: "143px", fontSize: "60px", transition: "all 1s ease-out" }}
+                  placeholder={placeholder}
+                  title="Type a letter"
+                />
+                <button
+                  className={
+                    "btn btn-lg fw-bold border-0 py-3 mx-auto btn" +
+                    isElementActive(!isValueEmpty) +
+                    noValue
+                  }
+                  type="submit"
+                  disabled={isLoading || isValueEmpty}
+                  title="Try"
+                >
+                  Guess letter
+                </button>
+              </div>
+            </form>
+          </>
+        )}
       </div>
-        <p className="lead mb-0">
-          Tried {onlyLetters(historic).length} of 8 letters
-        </p>
-        <p className="lead text-muted">({historic}...)</p>
+      <Errors showStage={showStage} message={message} historic={historic} isError={isError} />
     </div>
   );
 };
